64ed864a84de0a8df6b8b8a24d7871456920d7cd
perillaroc <perillaroc@gmail.com>
2025-06-03 10:55:48 +0800

    :loud_sound: add forecast time in log.
    :recycle: combine process codes for FileName and FileNames.

--- common/local.go
+++ common/local.go
@@ -10,40 +10,24 @@ import (
 )
 
 func FindLocalFile(config DataConfig, locationLevels []string, startTime time.Time, forecastTime time.Duration) PathItem {
+
 	tpVar := GenerateTimeTemplateVariable(startTime, forecastTime)
 
-	var fileNames []string
+	forecastTimeStr := FormatForecastTimeShort(forecastTime)
+	currentLog := log.WithFields(log.Fields{"forecastTime": forecastTimeStr})
 
+	var configFileNames []string
 	if len(config.FileNames) > 0 {
-		log.Debugf("using file_names...\n")
-		for _, item := range config.FileNames {
-			fileNameTemplate := template.Must(template.New("fileName").Funcs(template.FuncMap{
-				"generateStartTime":    GenerateStartTime,
-				"getYear":              GetYear,
-				"getMonth":             GetMonth,
-				"getDay":               GetDay,
-				"getHour":              GetHour,
-				"generateForecastTime": GenerateForecastTime,
-				"getForecastHour":      GetForecastHour,
-				"getForecastMinute":    GetForecastMinute,
-			}).Delims("{", "}").Parse(item))
-			var fileNameBuilder strings.Builder
-			err := fileNameTemplate.Execute(&fileNameBuilder, tpVar)
-			if err != nil {
-				log.Errorf("file name template execute has error: %s", err)
-				return PathItem{
-					Path:     config.Default,
-					PathType: config.Default,
-				}
-			}
-			fileName := fileNameBuilder.String()
-			fileNames = append(fileNames, fileName)
-			log.Debugf("find file name %s\n", fileName)
-		}
+		currentLog.Debugf("using file_names...")
+		configFileNames = config.FileNames
+	}
+	if len(config.FileName) > 0 {
+		currentLog.Debugf("using file_name...")
+		configFileNames = append(configFileNames, config.FileName)
 	}
 
-	if len(config.FileName) > 0 {
-		log.Debugf("using file_name...\n")
+	var fileNames []string
+	for _, item := range configFileNames {
 		fileNameTemplate := template.Must(template.New("fileName").Funcs(template.FuncMap{
 			"generateStartTime":    GenerateStartTime,
 			"getYear":              GetYear,
@@ -53,11 +37,11 @@ func FindLocalFile(config DataConfig, locationLevels []string, startTime time.Ti
 			"generateForecastTime": GenerateForecastTime,
 			"getForecastHour":      GetForecastHour,
 			"getForecastMinute":    GetForecastMinute,
-		}).Delims("{", "}").Parse(config.FileName))
+		}).Delims("{", "}").Parse(item))
 		var fileNameBuilder strings.Builder
 		err := fileNameTemplate.Execute(&fileNameBuilder, tpVar)
 		if err != nil {
-			log.Errorf("file name template execute has error: %s", err)
+			currentLog.Errorf("file name template execute has error: %s", err)
 			return PathItem{
 				Path:     config.Default,
 				PathType: config.Default,
@@ -65,7 +49,7 @@ func FindLocalFile(config DataConfig, locationLevels []string, startTime time.Ti
 		}
 		fileName := fileNameBuilder.String()
 		fileNames = append(fileNames, fileName)
-		log.Debugf("find file name %s\n", fileName)
+		currentLog.Debugf("find file name: %s", fileName)
 	}
 
 	for _, item := range config.Paths {
@@ -74,28 +58,31 @@ func FindLocalFile(config DataConfig, locationLevels []string, startTime time.Ti
 		locationLevelType := item.LevelType
 
 		if !inLocationLevelTypes(locationLevels, locationLevelType) {
-			log.Debugf("skip location level type %s\n", locationLevelType)
+			currentLog.Debugf("skip location level type %s for %s", locationLevelType, path)
 			continue
 		}
 
 		dirPathTemplate := template.Must(template.New("dirPath").Delims("{", "}").Parse(path))
-
 		var dirPathBuilder strings.Builder
 		err := dirPathTemplate.Execute(&dirPathBuilder, tpVar)
 		if err != nil {
-			log.Errorf("dir path template execute has error: %s", err)
+			currentLog.Errorf("dir path template execute has error: %s", err)
 			continue
 		}
+
 		dirPath := dirPathBuilder.String()
 		for _, fileName := range fileNames {
 			filePath := filepath.Join(dirPath, fileName)
-			//log.Infof("%s\n", filePath)
+			currentLog.Debugf("find file path: %s", filePath)
 
 			if CheckLocalFile(filePath) {
+				currentLog.Debugf("check file path success: %s", filePath)
 				return PathItem{
 					Path:     filePath,
 					PathType: pathType,
 				}
+			} else {
+				currentLog.Debugf("check file path failed: %s", filePath)
 			}
 		}
 	}
@@ -108,10 +95,8 @@ func FindLocalFile(config DataConfig, locationLevels []string, startTime time.Ti
 
 func CheckLocalFile(filePath string) bool {
 	if _, err := os.Stat(filePath); err != nil {
-		log.Debugf("check file path failed: %s", filePath)
 		return false
 	}
-	log.Debugf("check file path success: %s", filePath)
 	return true
 }
 
